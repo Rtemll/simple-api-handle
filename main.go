ce82bb189b06da932c7ded4e76e0e0e4267b74ac
1 <a1@MacBook-Pro.netis.cc>
2024-12-02 23:09:27 +0300

    second commit

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
 )
-
+var task string
 func HelloHandler(w http.ResponseWriter, r *http.Request){
-fmt.Fprintln(w, "Hello, World!")
+fmt.Fprint(w, "Hello, "+task+"!")
 }
-
+func PostTaskHandler(w http.ResponseWriter, r *http.Request){
+	var newTask struct{
+		Task string `json:"task"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&newTask)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	task = newTask.Task
+	fmt.Fprint(w, "Task update successfully")
+}
 func main(){
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/task", PostTaskHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
