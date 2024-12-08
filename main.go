1868cda63cea21e8652b0e55fd7640b2a9087fea
1 <a1@MacBook-Pro.netis.cc>
2024-12-08 22:56:45 +0300

    3 commit

--- main.go
+++ main.go
@@ -8,23 +8,37 @@ import (
 )
 var task string
 func HelloHandler(w http.ResponseWriter, r *http.Request){
-fmt.Fprint(w, "Hello, "+task+"!")
+	var messages []Message
+	DB.Find(&messages) // Запрос всех сообщений из базы данных
+	// Преобразование слайса в JSON
+	jsonData, err := json.Marshal(messages)
+	if err != nil {
+	 http.Error(w, err.Error(), http.StatusInternalServerError)
+	 return
+	}
+   
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
 func PostTaskHandler(w http.ResponseWriter, r *http.Request){
-	var newTask struct{
-		Task string `json:"task"`
-	}
-	err := json.NewDecoder(r.Body).Decode(&newTask)
-	if err != nil{
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	task = newTask.Task
-	fmt.Fprint(w, "Task update successfully")
+	var newTask Message // Используем структуру Message
+ err := json.NewDecoder(r.Body).Decode(&newTask)
+ if err != nil {
+  http.Error(w, err.Error(), http.StatusBadRequest)
+  return
+ }
+
+ DB.Create(&newTask) // Сохранение в базу данных
+ fmt.Fprint(w, "Task added successfully")
 }
 func main(){
+		// Вызываем метод InitDB() из файла db.go
+		InitDB()
+		// Автоматическая миграция модели Message 
+	DB.AutoMigrate(&Message{})
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
 	router.HandleFunc("/api/task", PostTaskHandler).Methods("POST")
+	fmt.Println("Server listening on port 8080") // Добавлено для лучшей обратной связи
 	http.ListenAndServe(":8080", router)
 }
